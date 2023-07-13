fafb00d897541e0809fd2298fa55eaecfcf1b375
Isabel López <isabellacero17@gmail.com>
2023-07-13 14:35:50 -0600

    Hica cambio materias

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from entidades.estudiante import Estudiante
+from entidades.materias import Materia
 app = Flask(__name__)
 
 
@@ -10,7 +11,7 @@ def index():
 
 @app.route('/estudiantes')
 def estudiantes():
-    # leer los estudaintes de la base de datos
+    # leer los estudiantes de la base de datos
     estudiantes = []
     with open('db/estudiantes.txt', 'r') as f:
         lines = f.read().splitlines()
@@ -22,6 +23,20 @@ def estudiantes():
 
     return render_template('estudiantes.html', data=estudiantes)
 
+@app.route('/materias')
+def materias():
+    # leer las materias de la base de datos
+    lista_materias = []
+    with open('db/materias.txt', 'r') as f:
+        lines = f.read().splitlines()
+        for line in lines:
+            print(line)
+            datos_materias = line.split('|')
+            materias = Materia(datos_materias)
+            lista_materias.append(materias)
+
+    return render_template('materias.html', data=lista_materias)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888, debug=True)
+    
---
+++ entidades/materias.py
@@ -0,0 +1,7 @@
+class Materia:
+    def __init__(self, materia):
+        self.nombre = materia
+        
+
+      
+    
